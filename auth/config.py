bad7a63fdf0b8d2e137a275005033cf06d5230e8
root <root@velora.192.168.1.53>
2026-07-14 14:40:08 +0000

    user menu fixed

--- auth/config.py
+++ auth/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+psycopg://muzic:gateless@192.168.1.95:5432/muzic"
+    DATABASE_URL: str = "postgresql+psycopg://muzic_user:gateless@192.168.1.95:5432/muzic"
     SECRET_KEY: str = "Um0EWdzOttS9-yFShwUpXEmvnV0Rkbe1VF6K4GYHrLS5J1xxca7Jc1Vycj1PPABCfq0kCKtwthanigai"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
